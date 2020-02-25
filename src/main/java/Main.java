c54e01ad9d2c2ca11def35b58cb3217e28131c5b
yixiaoming <yixiaoming.me@qq.com>
2020-02-25 11:17:42 +0800

    清空Main文件

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,57 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        Solution s = new Solution();
-        char[][] chars = new char[][]{
-                {'a'}
-        };
-        System.out.println(s.exist(chars, "ab"));
+
     }
 }
 
 class Solution {
-
-    int[][] directions = new int[][]{
-            {-1, 0}, {0, 1}, {1, 0}, {0, -1}
-    };
-
-    boolean[][] marks;
-
-    int rows;
-    int cols;
-
-    public boolean exist(char[][] board, String word) {
-        rows = board.length;
-        cols = board[0].length;
-        marks = new boolean[rows][cols];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (dfs(board, i, j, word, 0)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean inArea(int row, int col) {
-        return row >= 0 && row < rows && col >= 0 && col < cols;
-    }
-
-    public boolean dfs(char[][] board, int row, int col, String findStr, int index) {
-        if (index == findStr.length() - 1) return board[row][col] == findStr.charAt(index);
-        if (!inArea(row, col)) return false;
-        if (board[row][col] == findStr.charAt(index)) {
-            marks[row][col] = true;
-            for (int[] direction : directions) {
-                int newRow = row + direction[0];
-                int newCol = col + direction[1];
-                if (inArea(newRow, newCol) && marks[newRow][newCol] && dfs(board, newRow, newCol, findStr, index + 1)) {
-                    return true;
-                }
-            }
-            marks[row][col] = false;
-        }
-        return false;
-    }
 }
